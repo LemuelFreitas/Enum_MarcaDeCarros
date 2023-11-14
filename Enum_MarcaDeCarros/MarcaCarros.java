e49ef04cadc84ac29dc611684dcf35d6a9ea4d3a
LemuelFreitas <132360202+LemuelFreitas@users.noreply.github.com>
2023-11-14 11:30:28 -0300

    Add files via upload

---
+++ Enum_MarcaDeCarros/MarcaCarros.java
@@ -0,0 +1,14 @@
+package Enum_MarcaDeCarros;
+
+public enum MarcaCarros {
+HONDA,
+CHEVROLET,
+TOYOTA,
+BMW,
+PEUGEOT,
+AUDI,
+NISSAN,
+HYUNDAI,
+JAGUAR,
+PORSCHE
+}
---
+++ Enum_MarcaDeCarros/PrincipalEnum.java
@@ -0,0 +1,30 @@
+package Enum_MarcaDeCarros;
+
+public class PrincipalEnum {
+
+	public static void main(String[] args) {
+		MarcaCarros marca1 = MarcaCarros.AUDI;
+		MarcaCarros marca2 = MarcaCarros.BMW;
+		MarcaCarros marca3 = MarcaCarros.CHEVROLET;
+		MarcaCarros marca4 = MarcaCarros.HONDA;
+		MarcaCarros marca5 = MarcaCarros.HYUNDAI;
+		MarcaCarros marca6 = MarcaCarros.JAGUAR;
+		MarcaCarros marca7 = MarcaCarros.NISSAN;
+		MarcaCarros marca8 = MarcaCarros.PEUGEOT;
+		MarcaCarros marca9 = MarcaCarros.PORSCHE;
+		MarcaCarros marca10 = MarcaCarros.TOYOTA;
+		
+		System.out.println(marca1);
+		System.out.println(marca2);
+		System.out.println(marca3);
+		System.out.println(marca4);
+		System.out.println(marca5);
+		System.out.println(marca6);
+		System.out.println(marca7);
+		System.out.println(marca8);
+		System.out.println(marca9);
+		System.out.println(marca10);
+	}
+	
+
+}
